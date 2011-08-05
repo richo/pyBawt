b5f2ffac8f00f56f639483d85ef8f3f381ff0cbb
richo <richo@psych0tik.net>
2011-08-05 17:25:56 +1000

    Started framworking

--- ourgit.py
+++ ourgit.py
@@ -1,8 +1,22 @@
 import subprocess as sp
 
-def update_git():
-    p = sp.Popen(["/usr/bin/git", "fetch"], stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
+def current_branch():
+    p = sp.Popen(["/usr/bin/git", "branch"], stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
     (stdout, stdin) = (p.stdout, p.stdin)
+    while True:
+        i = stdout.readline()
+        if i.startswith("*"):
+            return i[1:].strip()
+    return False
+#   TODO
+        
+def update_git():
+    branch = current_branch()
+    p = sp.Popen(["/usr/bin/git", "pull"], stdout=sp.PIPE, close_fds=True)
+    #(stdout, stdin) = (p.stdout, p.stdin)
+    p.wait()
+    # TODO sanity check
+
 
 def checkout_git(branch):
     pass
@@ -11,10 +25,12 @@ def update_git_head():
     pass
 
 def version():
-    pass
+    p = sp.Popen(["/usr/bin/git", "show", "--shortstat", "HEAD"], stdout=sp.PIPE, close_fds=True)
+    stdout = p.stdout
+    return stdout.readline()[:16]
 
 def log():
-    pass
+    return ""
 
 def clean_git():
     pass
