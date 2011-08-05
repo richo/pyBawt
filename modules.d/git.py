fccbf65e5139bab7de4d3986ef1445059cc54637
richo <richo@psych0tik.net>
2011-08-06 00:38:16 +1000

    Need git in the command

--- modules.d/git.py
+++ modules.d/git.py
@@ -2,7 +2,7 @@ import ourgit
 
 class GitModule(BawtM2):
     _name = "GitModule"
-    privmsg_re = "^(!|%(nick)s:\s+)([^ ].*)"
+    privmsg_re = "^(!|%(nick)s:\s+)git ?([^ ].*)"
     def handle_privmsg(self, msg):
         cmd = self.m.group(2)
         if cmd:
