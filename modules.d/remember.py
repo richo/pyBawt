97acd9c3e355b25f0fb64f619f136e5071333313
richo <richo@psych0tik.net>
2011-08-11 00:20:27 +1000

    Fuck, by now you'd think I'd be on this

--- modules.d/remember.py
+++ modules.d/remember.py
@@ -48,7 +48,7 @@ class NoteModule(BawtM2):
                     self.reply(msg, "!store <note>")
             elif self.m.group(2) == "clear":
                 while notes:
-                    notes.remove(0)
+                    del notes[0]
                 self.reply(msg, "Message list cleared.")
             elif self.m.group(2) == "list":
                 if not notes:
