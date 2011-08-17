7f15cf91c5c262292e5e9bcfe3e952a0faab7683
richo <richo@psych0tik.net>
2011-08-18 08:23:30 +1000

    Git is a guinea pig for the new auth framework

--- modules.d/git.py
+++ modules.d/git.py
@@ -4,7 +4,9 @@ class GitModule(BawtM2):
     _name = "GitModule"
     privmsg_re = "^(!|%(nick)s:\s+)git ?([^ ].*)"
     def handle_privmsg(self, msg):
-        cmd = 'git ' + self.m.group(2)
-        if cmd:
-            self.parent.privmsg(msg.replyto, ourgit.oneline(cmd))
-    
+        if parent.Authenticator.authed(msg.nick):
+            cmd = 'git ' + self.m.group(2)
+            if cmd:
+                self.parent.privmsg(msg.replyto, ourgit.oneline(cmd))
+        else:
+            self.parent.privmsg(msg.replyto, "You are not authenticated")
