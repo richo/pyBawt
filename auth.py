9ca222e572f18784266de937a3a58032809104bd
richo <richo@psych0tik.net>
2011-08-18 08:35:14 +1000

    Started on an auth lauer which also stores files, not really sure how it'll work

--- auth.py
+++ auth.py
@@ -9,12 +9,16 @@ It is managed by AuthModule which tracks the server messages
         self.auth_hash = auth_hash
         self.valid_host = valid_host
         self.authenticated = []
+        self.storage = {}
 
     def try_auth(self, msg, password):
         if msg.nick in self.authenticated:
             return True
         if hashlib.md5(password).hexdigest() == self.auth_hash:
             self.authenticated.append(msg.nick)
+            # TODO
+            # Storage should itself be persistant
+            storage[msg.nick] = {}
             return True
         return False
 
