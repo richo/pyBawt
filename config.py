088d6ada0f25d43c1a03cb5ae063300d1d9df1d2
richo <richo@psych0tik.net>
2011-08-17 21:32:15 +1000

    Merged network config into config

---
+++ config.py
@@ -0,0 +1,9 @@
+# TODO Documentation
+# dsl ruby style crap, this shouldn't need quotes etc
+host            =   'irc.psych0tik.net'
+ssl             =   True
+nick            =   'pyBawt'
+port            =   6697
+auth_host       =   'staffers.psych0tik.net'
+auth_hash       =   '965cc7027c64265a2e4bd602dfcc85b1'
+
