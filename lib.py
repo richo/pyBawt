f6686c59e81217f13dcad67df17943f03f0d866b
richo <richo@psych0tik.net>
2011-08-24 08:32:22 +1000

    Mapping is now extendable by specifying what type of objects you want in a subclass

--- lib.py
+++ lib.py
@@ -1,7 +1,8 @@
 class Mapping(dict):
+    missing_elements = list
     def __getitem__(self, item):
         try:
             return super(Mapping, self).__getitem__(item)
         except KeyError:
-            self[item] = []
+            self[item] = self.missing_elements()
             return self[item]
