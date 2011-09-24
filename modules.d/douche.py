6d3a9f6e6709b2f153bbaaa2b4d3d57d9dc93400
richo <richo@psych0tik.net>
2011-09-24 16:40:31 +1000

    Moved a shitton of modules to the private namespace

--- modules.d/douche.py
+++
@@ -1,8 +0,0 @@
-class DoucheModule(BawtM2):
-    privmsg_re = "!douche"
-    _name = "DoucheModule"
-    def handle_privmsg(self, msg):
-        args = msg.data_segment.split(" ")
-        if len(args) > 1:
-            self.parent.privmsg(msg.replyto, "%s: Stop being a douche" % (args[1]))
-
